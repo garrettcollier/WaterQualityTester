579ddd45356cb7e3f52b75a264fe6d208632e712
Garrett <garrettcollierbusiness@gmail.com>
2022-10-28 23:15:47 -0500

    Setup Page Navigation and Map defaulted at LR, AR location

--- water_quality_app/ios/Runner/AppDelegate.swift
+++ water_quality_app/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,10 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    // Google Maps API key
+    GMSServices.provideAPIKey("AIzaSyB3Z0znl6BHklEmmr9gR6Nz7addXInVzwY")
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
