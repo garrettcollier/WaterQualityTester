5eeed05c8a690303113d15fb904f81a243a43f45
Alexandria <alexandriacade0@gmail.com>
2022-11-10 22:40:45 -0600

    Buttons with epa standards
    All buttons have alert dialogs

--- water_quality_app/lib/epaStandards.dart
+++ water_quality_app/lib/epaStandards.dart
@@ -1,14 +1,12 @@
 import 'dart:collection';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class EPAStandards extends StatelessWidget {
   EPAStandards({super.key});
 
-  final standards = <String, String>{
-    "pH":
-        "U.S. EPA water quality criteria for pH in freshwater suggest a range of 6.5 to 9."
-  };
+  final standard = "";
 
   @override
   Widget build(BuildContext context) {
@@ -23,27 +21,124 @@ class EPAStandards extends StatelessWidget {
                       context: context,
                       builder: (BuildContext context) {
                         return const AlertDialog(
-                          title: Text("pH"), //https://www.epa.gov/caddis-vol2/ph#:~:text=U.S.%20EPA%20water%20quality%20criteria,range%20of%206.5%20to%209.
-                          content: Text(
-                              "U.S. EPA water quality criteria for pH in freshwater suggest a range of 6.5 to 9."),
+                          title: Text("pH"),
+                          content: Text("EPA Standard Recommended Limit: 6.5 - 8.5. \nLow pH causes bitter metalic taste. \nHigh pH causes slippery feel."),
                         );
                       });
                 },
                 child: Text("pH")),
-            TextButton(onPressed: () {}, child: Text("Hydrogen Sulfide")),
-            TextButton(onPressed: () {}, child: Text("Iron")),
-            TextButton(onPressed: () {}, child: Text("Copper")),
-            TextButton(onPressed: () {}, child: Text("Lead")),
-            TextButton(onPressed: () {}, child: Text("Manganese")),
-            TextButton(onPressed: () {}, child: Text("Total Chlorine")),
-            TextButton(onPressed: () {}, child: Text("Free Chlorine")),
-            TextButton(onPressed: () {}, child: Text("Nitrate")),
-            TextButton(onPressed: () {}, child: Text("Nitrite")),
-            TextButton(onPressed: () {}, child: Text("Sulfate")),
-            TextButton(onPressed: () {}, child: Text("Zinc")),
-            TextButton(onPressed: () {}, child: Text("Flouride")),
-            TextButton(onPressed: () {}, child: Text("Sodium Chloride")),
-            TextButton(onPressed: () {}, child: Text("Total Alkalinity")),
+            TextButton(onPressed: () {showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return const AlertDialog(
+                          title: Text("Hydrogen Sulfide"),
+                          content: Text("EPA Standard Recommended Limit: 0 mg/L. \nCauses rotten egg odor and foul taste"),
+                        );
+                      });}, child: Text("Hydrogen Sulfide")),
+            TextButton(onPressed: () {showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return const AlertDialog(
+                          title: Text("Iron"),
+                          content: Text("EPA Standard Recommended Limit: 0.3 mg/L \nCauses rusty color and staining, metallic taste"),
+                        );
+                      });}, child: Text("Iron")),
+            TextButton(onPressed: () {showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return const AlertDialog(
+                          title: Text("Copper"),
+                          content: Text("EPA Standard Recommended Limit: 1.3 mg/L. \nCauses liver and kidney damage"),
+                        );
+                      });}, child: Text("Copper")),
+            TextButton(onPressed: () {showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return const AlertDialog(
+                          title: Text("Lead"),
+                          content: Text("EPA Standard Recommended Limit: 0.015 mg/L. \nCauses deficit in learning ability for children, kidney problems, and high blood pressure in adults."),
+                        );
+                      });}, child: Text("Lead")),
+            TextButton(onPressed: () {showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return const AlertDialog(
+                          title: Text("Manganese"),
+                          content: Text("EPA Standard Recommended Limit: 0.05 mg/L. \nCauses black/brown color, bitter metallic taste"),
+                        );
+                      });}, child: Text("Manganese")),
+            TextButton(onPressed: () {showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return const AlertDialog(
+                          title: Text("Total Chlorine"),
+                          content: Text("EPA Standard Recommended Limit: 4 mg/L. \nCauses skin irritation and stomach discomfort"),
+                        );
+                      });}, child: Text("Total Chlorine")),
+            TextButton(onPressed: () {showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return const AlertDialog(
+                          title: Text("Free Chlorine"),
+                          content: Text("EPA Standard Recommended Limit: . \nCauses "),
+                        );
+                      });}, child: Text("Free Chlorine")),
+            TextButton(onPressed: () {showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return const AlertDialog(
+                          title: Text("Nitrate"),
+                          content: Text("EPA Standard Recommended Limit: 10 mg/L. \nCauses Blue Baby syndrome in infants under 6 months, shortness of breath, nausea, and dizziness"),
+                        );
+                      });}, child: Text("Nitrate")),
+            TextButton(onPressed: () {showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return const AlertDialog(
+                          title: Text("Nitrite"),
+                          content: Text("EPA Standard Recommended Limit: 1 mg/L. \nCauses Blue Baby Syndrome in infants under 6 months, shortness of breath, nausea, and dizziness"),
+                        );
+                      });}, child: Text("Nitrite")),
+            TextButton(onPressed: () {showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return const AlertDialog(
+                          title: Text("Sulfate"),
+                          content: Text("EPA Standard Recommended Limit: 250 mg/L. \nCauses salty taste"),
+                        );
+                      });}, child: Text("Sulfate")),
+            TextButton(onPressed: () {showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return const AlertDialog(
+                          title: Text("Zinc"),
+                          content: Text("EPA Standard Recommended Limit: 5 mg/L. \nCauses metallic taste"),
+                        );
+                      });}, child: Text("Zinc")),
+            TextButton(onPressed: () {showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return const AlertDialog(
+                          title: Text("Flouride"),
+                          content: Text("EPA Standard Recommended Limit: 4 mg/L. \nCauses bone disease and mottied teeth"),
+                        );
+                      });}, child: Text("Flouride")),
+            TextButton(onPressed: () {showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return const AlertDialog(
+                          title: Text("Sodium Chloride"),
+                          content: Text("EPA Standard Recommended Limit: 250 mg/L. \nCauses bloating, swollen hands and feet"),
+                        );
+                      });}, child: Text("Sodium Chloride")),
+            TextButton(onPressed: () {showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return const AlertDialog(
+                          title: Text("Total Alkalinity"),
+                          content: Text("EPA Standard Recommended Limit: 75-150 mg/L. \nLow alkalinity associated with acidic pH and corrosion"),
+                        );
+                      });}, child: Text("Total Alkalinity")),
           ],
         ),
       ),
