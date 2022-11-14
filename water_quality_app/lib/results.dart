b2abe4ecd63eb079345f16e55b605086d456f5fb
Gunner <hopkinsgd@hendrix.edu>
2022-11-14 12:08:53 -0600

    PH_Color Samples Inserted

---
+++ water_quality_app/lib/results.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:water_quality_app/image_with_rgb.dart';
+
+class ResultsPage extends StatelessWidget {
+  final File image;
+
+  const ResultsPage({super.key, required this.image});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Results'),
+      ),
+      body: RGB(image: image),
+    );
+  }
+}
