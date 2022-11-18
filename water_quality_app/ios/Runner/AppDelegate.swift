550516ae652b6a9f0c2174a992355b0102e26bf6
Gunner <hopkinsgd@hendrix.edu>
2022-11-18 14:33:51 -0600

    Final Changes

--- water_quality_app/ios/Runner/AppDelegate.swift
+++ water_quality_app/ios/Runner/AppDelegate.swift
@@ -11,7 +11,7 @@ import GoogleMaps
     GeneratedPluginRegistrant.register(with: self)
 
     // Google Maps API key
-    GMSServices.provideAPIKey("")
+    GMSServices.provideAPIKey("Enter_Key_Here")
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
