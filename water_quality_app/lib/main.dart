0e922890347239987afe5fa7b7ef3b0860c2464c
Alexandria <alexandriacade0@gmail.com>
2022-11-21 15:06:13 -0600

    Initialize Firebase in app
    Importing Firebase packages

--- water_quality_app/lib/main.dart
+++ water_quality_app/lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'package:water_quality_app/begin.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 List<CameraDescription> cameras = [];
 Future<void> main() async {
   try {
     WidgetsFlutterBinding.ensureInitialized();
     cameras = await availableCameras();
+    await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   } on CameraException catch (e) {
     print('Error in fetching the cameras: $e');
   }
