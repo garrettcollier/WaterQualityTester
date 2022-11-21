1902d38094beb941ea041b09b67315ddafb271fe
Alexandria <alexandriacade0@gmail.com>
2022-11-21 15:24:17 -0600

    Firebase Collections
    Initializing collections for each standard

---
+++ water_quality_app/lib/firebase.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:water_quality_app/begin.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+//init database collections
+final pHCollection = FirebaseFirestore.instance.collection('pH');
+final ZincCollection = FirebaseFirestore.instance.collection('Zinc');
+final TotalChlorineCollection = FirebaseFirestore.instance.collection('TotalChlorine');
+final TotalAlkalineCollection = FirebaseFirestore.instance.collection('TotalAlkaline');
+final SulfateCollection = FirebaseFirestore.instance.collection('Sulfate');
+final SodiumChlorideCollection = FirebaseFirestore.instance.collection('SodiumChloride');
+final NitriteCollection = FirebaseFirestore.instance.collection('Nitrite');
+final NitrateCollection = FirebaseFirestore.instance.collection('Nitrate');
+final ManganeseCollection = FirebaseFirestore.instance.collection('Manganese');
+final LeadCollection = FirebaseFirestore.instance.collection('Lead');
+final HydrogenSulfideCollection = FirebaseFirestore.instance.collection('HydrogenSulfide');
+final FreeChlorineCollection = FirebaseFirestore.instance.collection('FreeChlorine');
+final FlourideCollection = FirebaseFirestore.instance.collection('Flouride');
+final CopperCollecction = FirebaseFirestore.instance.collection('Copper');
--- water_quality_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ water_quality_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,11 +5,13 @@
 import FlutterMacOS
 import Foundation
 
+import cloud_firestore
 import firebase_core
 import geolocator_apple
 import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
