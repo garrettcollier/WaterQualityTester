127c14fb9c01191baa066dfca5df7d1591da1c68
Garrett <colliergc@hendrix.edu>
2022-11-20 19:07:22 -0600

    Reformat image display to show image, image pixels, and test kit pixels

--- water_quality_app/lib/image_with_rgb.dart
+++ water_quality_app/lib/image_with_rgb.dart
@@ -21,7 +21,8 @@ int noOfPaletteColors = 16;
 
 class _RGBState extends State<RGB> {
   // lists for colors, sorted colors, and palette of colors
-  List<Color> colors = [];
+  List<Color> allColors = [];
+  List<Color> testColors = [];
   List<Color> sortedColors = [];
   List<Color> palette = [];
 
@@ -58,38 +59,34 @@ class _RGBState extends State<RGB> {
                   ],
                 ),
         ),
-        child: ListView(
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            // display image
-            SizedBox(
-              height: 300,
-              child: imageBytes != null && imageBytes!.isNotEmpty
-                  ? Image.memory(
-                      imageBytes!,
-                      fit: BoxFit.fill,
-                    )
-                  : const Center(child: CircularProgressIndicator()),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            // display palette
-            Container(
-              color: Colors.white.withOpacity(0.5),
-              padding: const EdgeInsets.only(top: 6, bottom: 16),
-              alignment: Alignment.center,
-              child: Column(
-                children: [
-                  Text('Palette of $noOfPaletteColors colors:'),
-                  const SizedBox(height: 10),
-                  _getPalette()
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 20,
               ),
-            )
-          ],
+              // display image
+              Align(
+                alignment: Alignment.center,
+                child: SizedBox(
+                  height: (MediaQuery.of(context).size.height),
+                  width: MediaQuery.of(context).size.width / 2,
+                  child: imageBytes != null && imageBytes!.isNotEmpty
+                      ? Image.memory(
+                          imageBytes!,
+                          fit: BoxFit.fill,
+                        )
+                      : const Center(child: CircularProgressIndicator()),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              // get image color pixels and just the lightest color column
+              _getColorGrid(true),
+              _getColorGrid(false)
+            ],
+          ),
         ),
       ),
     );
@@ -98,7 +95,7 @@ class _RGBState extends State<RGB> {
   // get all colors from image
   Future<void> extractColors() async {
     // initial colors, sorted colors, palette, and image bytes
-    colors = [];
+    allColors = [];
     sortedColors = [];
     palette = [];
     imageBytes = null;
@@ -110,83 +107,74 @@ class _RGBState extends State<RGB> {
 
     // get image bytes and extract colors, sort, and generate the palette
     imageBytes = await widget.image.readAsBytes();
-    colors = await compute(extractPixelsColors, imageBytes);
+    allColors = await compute(extractPixelsColors, imageBytes);
+
+    // get just the water test kit colors
+    testColors = getSingleColumnFromListIndex(
+        allColors, getLighestColorIndex(allColors));
+
     setState(() {});
-    sortedColors = await compute(sortColors, colors);
+    sortedColors = await compute(sortColors, allColors);
     setState(() {});
-    palette = await compute(
-        generatePalette, {keyPalette: colors, keyNoOfItems: noOfPaletteColors});
+    palette = await compute(generatePalette,
+        {keyPalette: allColors, keyNoOfItems: noOfPaletteColors});
     primary = palette.last;
     primaryText = palette.first;
-    background = palette.first.withOpacity(0.5);
     setState(() {});
   }
 
-  // widget to get the palette
-  Widget _getPalette() {
+// displays grid of all pixels from image
+  Widget _getColorGrid(bool allColorsBool) {
+    var colors = [];
+    var titleText = "";
+    if (allColorsBool) {
+      colors = allColors;
+      titleText = "Colors from Picture";
+    } else {
+      colors = testColors;
+      titleText = "Test Kit Colors";
+    }
     return SizedBox(
-      height: 50,
-      child: palette.isEmpty
-          ? Container(
-              alignment: Alignment.center,
-              height: 100,
-              child: const CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              shrinkWrap: true,
-              scrollDirection: Axis.horizontal,
-              itemCount: palette.length,
-              itemBuilder: (BuildContext context, int index) => Container(
-                color: palette[index],
-                height: 50,
-                width: 50,
-              ),
-            ),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: colors.isEmpty
+                ? Container(
+                    alignment: Alignment.center,
+                    height: 200,
+                    child: const CircularProgressIndicator(),
+                  )
+                : Column(
+                    children: [
+                      Text(titleText),
+                      const SizedBox(height: 10),
+                      GridView.builder(
+                        shrinkWrap: true,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: noOfPixelsPerAxis),
+                        itemCount: colors.length,
+                        itemBuilder: (BuildContext ctx, index) {
+                          return Container(
+                            alignment: Alignment.center,
+                            width: 25,
+                            child: Container(
+                              color: colors[index],
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+          ),
+        ],
+      ),
     );
   }
 }
 
-// GIVES GRIDS FOR ALL PIXELS IN IMAGE (SORTED AND UNSORTED)
+// GIVES GRIDS FOR ALL PIXELS IN IMAGE (SORTED)
 // SEEMS UNNEEDED BUT LEAVING HERE JUST IN CASE
-// Widget _getGrids() {
-//   return SizedBox(
-//     height: 260,
-//     child: Row(
-//       children: [
-//         Expanded(
-//           flex: 1,
-//           child: colors.isEmpty
-//               ? Container(
-//                   alignment: Alignment.center,
-//                   height: 200,
-//                   child: const CircularProgressIndicator(),
-//                 )
-//               : Column(
-//                   children: [
-//                     Text(
-//                       'Extracted Pixels',
-//                       style: TextStyle(
-//                           color:
-//                               palette.isEmpty ? Colors.black : palette.first),
-//                     ),
-//                     const SizedBox(height: 10),
-//                     GridView.builder(
-//                         shrinkWrap: true,
-//                         gridDelegate:
-//                             SliverGridDelegateWithFixedCrossAxisCount(
-//                                 crossAxisCount: noOfPixelsPerAxis),
-//                         itemCount: colors.length,
-//                         itemBuilder: (BuildContext ctx, index) {
-//                           return Container(
-//                             alignment: Alignment.center,
-//                             child: Container(
-//                               color: colors[index],
-//                             ),
-//                           );
-//                         }),
-//                   ],
-//                 ),
-//         ),
 //         const SizedBox(width: 10),
 //         Expanded(
 //           flex: 1,
@@ -226,3 +214,27 @@ class _RGBState extends State<RGB> {
 //     ),
 //   );
 // }
+
+// GETS PALETTE OF COLORS FROM IMAGE
+  // widget to get the palette
+  // Widget _getPalette() {
+  //   return SizedBox(
+  //     height: 50,
+  //     child: palette.isEmpty
+  //         ? Container(
+  //             alignment: Alignment.center,
+  //             height: 100,
+  //             child: const CircularProgressIndicator(),
+  //           )
+  //         : ListView.builder(
+  //             shrinkWrap: true,
+  //             scrollDirection: Axis.horizontal,
+  //             itemCount: palette.length,
+  //             itemBuilder: (BuildContext context, int index) => Container(
+  //               color: palette[index],
+  //               height: 50,
+  //               width: 50,
+  //             ),
+  //           ),
+  //   );
+  // }
