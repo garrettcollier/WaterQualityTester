6fde5bf5742251aa0681f170cd0075a83b685f61
Alexandria <alexandriacade0@gmail.com>
2022-11-21 14:57:25 -0600

    Installing packages

--- water_quality_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ water_quality_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_core
 import geolocator_apple
 import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
