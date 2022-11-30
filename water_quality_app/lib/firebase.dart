9f2e7554ae5d6e2c884205c6a3cfd6cc65cd7e8a
Alexandria <alexandriacade0@gmail.com>
2022-11-29 21:23:05 -0600

    Firebase setup

--- water_quality_app/lib/firebase.dart
+++ water_quality_app/lib/firebase.dart
@@ -6,20 +6,20 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 //initialize an instance of cloud firestore
-FirebaseFirestore db = FirebaseFirestore.instance;
+final db = FirebaseFirestore.instance;
 
 //init database collections
-final pHCollection = FirebaseFirestore.instance.collection('pH');
+final pHCollection = db.collection('pH');
 final ZincCollection = FirebaseFirestore.instance.collection('Zinc');
-final TotalChlorineCollection = FirebaseFirestore.instance.collection('TotalChlorine');
-final TotalAlkalineCollection = FirebaseFirestore.instance.collection('TotalAlkaline');
+final TotalChlorineCollection =FirebaseFirestore.instance.collection('TotalChlorine');
+final TotalAlkalineCollection =FirebaseFirestore.instance.collection('TotalAlkaline');
 final SulfateCollection = FirebaseFirestore.instance.collection('Sulfate');
-final SodiumChlorideCollection = FirebaseFirestore.instance.collection('SodiumChloride');
+final SodiumChlorideCollection =FirebaseFirestore.instance.collection('SodiumChloride');
 final NitriteCollection = FirebaseFirestore.instance.collection('Nitrite');
 final NitrateCollection = FirebaseFirestore.instance.collection('Nitrate');
 final ManganeseCollection = FirebaseFirestore.instance.collection('Manganese');
 final LeadCollection = FirebaseFirestore.instance.collection('Lead');
-final HydrogenSulfideCollection = FirebaseFirestore.instance.collection('HydrogenSulfide');
-final FreeChlorineCollection = FirebaseFirestore.instance.collection('FreeChlorine');
+final HydrogenSulfideCollection =FirebaseFirestore.instance.collection('HydrogenSulfide');
+final FreeChlorineCollection =FirebaseFirestore.instance.collection('FreeChlorine');
 final FlourideCollection = FirebaseFirestore.instance.collection('Flouride');
 final CopperCollection = FirebaseFirestore.instance.collection('Copper');
