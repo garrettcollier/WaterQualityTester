efaa8f1d91c81479153425a344f1bb9345735894
Garrett <colliergc@hendrix.edu>
2022-11-29 18:55:57 -0600

    Get user location, comment out image taken
    Added buttons for plotting location and then returning to home page as well as just returning to the home page

--- water_quality_app/lib/image_with_rgb.dart
+++ water_quality_app/lib/image_with_rgb.dart
@@ -40,60 +40,110 @@ class _RGBState extends State<RGB> {
   // image bytes
   Uint8List? imageBytes;
 
+  // style the elevated buttons
+  final ButtonStyle styleButton = ElevatedButton.styleFrom(
+      textStyle: const TextStyle(fontSize: 20), backgroundColor: Colors.teal);
+
   @override
   void initState() {
     super.initState();
     extractColors();
   }
 
+  // created method for getting user current location
+  Future<Position> getUserCurrentLocation() async {
+    await Geolocator.requestPermission()
+        .then((value) {})
+        .onError((error, stackTrace) async {
+      await Geolocator.requestPermission();
+      print("ERROR $error");
+    });
+    return await Geolocator.getCurrentPosition();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: background,
-      body: Container(
-        decoration: BoxDecoration(
-          // setup initial gradient
-          gradient: palette.isEmpty
-              ? null
-              : LinearGradient(
-                  begin: Alignment.bottomCenter,
-                  end: Alignment.topCenter,
-                  stops: const [0.01, 0.6, 1],
-                  colors: [
-                    palette.first,
-                    palette[palette.length ~/ 2],
-                    palette.last,
-                  ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+
+            // // display image
+            // Align(
+            //   alignment: Alignment.center,
+            //   child: SizedBox(
+            //     height: (MediaQuery.of(context).size.height),
+            //     width: MediaQuery.of(context).size.width / 2,
+            //     child: imageBytes != null && imageBytes!.isNotEmpty
+            //         ? Image.memory(
+            //             imageBytes!,
+            //             fit: BoxFit.fill,
+            //           )
+            //         : const Center(child: CircularProgressIndicator()),
+            //   ),
+            // ),
+            // const SizedBox(
+            //   height: 10,
+            // ),
+
+            // get image color pixels and just the lightest color column
+            _getColorGrid(true),
+            _getColorGrid(false),
+
+            // buttons for adding your location to
+            // the map and returning to home page
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  style: styleButton,
+                  child: const Text("Plot your Location"),
+                  onPressed: () async {
+                    getUserCurrentLocation().then(
+                      (value) async {
+                        print("${value.latitude} ${value.longitude}");
+
+                        // marker added for current users location
+                        // MARKER LIST CURRENTLY RESETS WHEN APP IS CLOSED
+                        markerList.add(
+                          Marker(
+                            markerId:
+                                MarkerId((markerList.length + 1).toString()),
+                            position: LatLng(value.latitude, value.longitude),
+                            infoWindow: const InfoWindow(
+                              title: 'My Current Location',
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                    // go to home page after plot
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const Home(),
+                      ),
+                    );
+                  },
                 ),
-        ),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 20,
-              ),
-              // display image
-              Align(
-                alignment: Alignment.center,
-                child: SizedBox(
-                  height: (MediaQuery.of(context).size.height),
-                  width: MediaQuery.of(context).size.width / 2,
-                  child: imageBytes != null && imageBytes!.isNotEmpty
-                      ? Image.memory(
-                          imageBytes!,
-                          fit: BoxFit.fill,
-                        )
-                      : const Center(child: CircularProgressIndicator()),
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              // get image color pixels and just the lightest color column
-              _getColorGrid(true),
-              _getColorGrid(false)
-            ],
-          ),
+                ElevatedButton(
+                    style: styleButton,
+                    child: const Text("Return to Home Page"),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const Front(),
+                        ),
+                      );
+                    }),
+              ],
+            ),
+          ],
         ),
       ),
     );
