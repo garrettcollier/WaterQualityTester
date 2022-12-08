fa36440fab2456b5fcc4e44eb7f718b09945b6a6
Gunner <hopkinsgd@hendrix.edu>
2022-12-07 18:41:43 -0600

    Added rows an implemented color and name list

--- water_quality_app/lib/results.dart
+++ water_quality_app/lib/results.dart
@@ -8,7 +8,9 @@ class ResultsPage extends StatelessWidget {
   final File image;
   List<String> namesList = ["Total Alkalinity", "Sodium Chloride", "Fluoride", "Zinc", "Sulfate", "Nitrite", "Nitrate", "Mercury", "Total Chlorine", "Manganese", "Lead", "Copper", "Iron", "Hydrogen Sulfide", "Hardness", "pH"]; 
   //RGB(image: image),
-  List<Color> colorList = [Colors.red, Colors.green, Colors.blue, Colors.black];
+  List<Color> colorList = [Colors.red, Colors.green, Colors.blue, Colors.black, Colors.red, Colors.red, Colors.red, Colors.red, Colors.red, Colors.red, Colors.red, Colors.red, Colors.red, Colors.red, Colors.red, Colors.red];
+
+  int index = 0;
 
   ResultsPage({super.key, required this.image});
 
@@ -22,13 +24,151 @@ class ResultsPage extends StatelessWidget {
       Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-           Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                
-              ],
-          )
-        ]
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                namesList[index]
+                ),
+              Text(
+                colorList[index].toString()
+                )],),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                namesList[index += 1]
+                ),
+              Text(
+                colorList[index += 1].toString()
+                )],),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                namesList[index += 2]
+                ),
+              Text(
+                colorList[index += 2].toString()
+                )],),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                namesList[index += 3]
+                ),
+              Text(
+                colorList[index += 3].toString()
+                )],),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                namesList[index += 4]
+                ),
+              Text(
+                colorList[index += 4].toString()
+                )],),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                namesList[index += 5]
+                ),
+              Text(
+                colorList[index += 5].toString()
+                )],),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                namesList[index += 6]
+                ),
+              Text(
+                colorList[index += 6].toString()
+                )],),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                namesList[index += 7]
+                ),
+              Text(
+                colorList[index += 7].toString()
+                )],),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                namesList[index += 8]
+                ),
+              Text(
+                colorList[index += 8].toString()
+                )],),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                namesList[index += 9]
+                ),
+              Text(
+                colorList[index += 9].toString()
+                )],),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                namesList[index += 10]
+                ),
+              Text(
+                colorList[index += 10].toString()
+                )],),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                namesList[index += 11]
+                ),
+              Text(
+                colorList[index += 11].toString()
+                )],),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                namesList[index += 12]
+                ),
+              Text(
+                colorList[index += 12].toString()
+                )],),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                namesList[index += 13]
+                ),
+              Text(
+                colorList[index += 13].toString()
+                )],),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                namesList[index += 14]
+                ),
+              Text(
+                colorList[index += 14].toString()
+                )],),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                namesList[index += 15]
+                ),
+              Text(
+                colorList[index += 15].toString()
+                )],),
+      ]
       )
       
 
