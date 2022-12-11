492dbe0198af332e984d6a01285c888e0463d75b
Gunner <hopkinsgd@hendrix.edu>
2022-12-11 15:08:09 -0600

    Changes

--- water_quality_app/lib/results.dart
+++ water_quality_app/lib/results.dart
@@ -71,7 +71,7 @@ class ResultsPage extends StatelessWidget {
   ResultsPage({super.key, required this.image});
 
   final TextStyle textstyle2 = GoogleFonts.oswald(
-    fontSize: 15,
+    fontSize: 14,
     textStyle: TextStyle(color: Colors.black, letterSpacing: .5));
 
   Widget columnFiller = Container(
